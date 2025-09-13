d954f8b3e169682582022a35841d0a8822a0ef74
WonJun Na <43125863+sladja3929@users.noreply.github.com>
2025-09-13 15:18:38 +0900

    ray tracing fphong.glsl 깨진 주석 제거 및 주석 업데이트

--- OpenGL/fphong.glsl
+++ OpenGL/fphong.glsl
@@ -1,5 +1,6 @@
 #version 330
 
+//w: 월드 좌표계로 계산된 View, Pos, Normal
 in vec3 N3; 
 in vec3 L3; 
 in vec3 V3; 
@@ -44,8 +45,11 @@ uniform vec4 uSpc;
 uniform float uShininess; 
 uniform samplerCube uCube;
 uniform vec4 uEPos;
+
+//구체 위치 정보가 유니폼으로 전달됨
 uniform int uNumSphere;
 uniform Sphere uSpheres[20];
+
 uniform int uBounceLimit;
 uniform int uDrawingMode;
 
@@ -53,7 +57,6 @@ uniform int uDrawingMode;
 
 bool IntersectRay( inout HitInfo hit, Ray ray );
 
-// Shades the given point and returns the computed color.
 vec4 Shade( Material mtl, vec4 position, vec3 normal, vec3 view, bool isFirst )
 {
 	vec4 color = vec4(0,0,0,1);
@@ -62,6 +65,7 @@ vec4 Shade( Material mtl, vec4 position, vec3 normal, vec3 view, bool isFirst )
 	sRay.pos = position.xyz;
 	sRay.dir = L;
 
+	//그림자가 지는 영역일 경우 ambient만 반환
 	HitInfo sHit;
 	if (IntersectRay(sHit, sRay))
 	{
@@ -69,13 +73,14 @@ vec4 Shade( Material mtl, vec4 position, vec3 normal, vec3 view, bool isFirst )
 		return color;
 	}
 
+	//색상 계산: 반사광일 경우 diffuse만 계산
 	float NL = max(dot(normal, L), 0.0);
 	if (!isFirst) {
 		color = uLIntensity * mtl.k_d * NL;
 		return color;
 	}
 
-	// Ã¹ ¹øÂ° shadeÀÏ °æ¿ì ambient, specular±îÁö °è»ê
+	//색상 계산: 첫 번째 발사한 광선일 경우 색상 전체를 계산
 	vec3 H = normalize(view + L);		
 	float VR = pow(max(dot(H, normal), 0), mtl.n);
 
@@ -83,10 +88,7 @@ vec4 Shade( Material mtl, vec4 position, vec3 normal, vec3 view, bool isFirst )
 	return color;
 }
 
-// Intersects the given ray with all spheres in the scene
-// and updates the given HitInfo using the information of the sphere
-// that first intersects with the ray.
-// Returns true if an intersection is found.
+//광선과 구의 방정식을 통해 교차점을 계산, 교차점이 2개일 경우 가까운 점을 반환
 bool IntersectRay( inout HitInfo hit, Ray ray )
 {
 	hit.t = 1e30;
@@ -116,28 +118,28 @@ bool IntersectRay( inout HitInfo hit, Ray ray )
 	return foundHit;
 }
 
-// Given a ray, returns the shaded color where the ray intersects a sphere.
-// If the ray does not hit a sphere, returns the environment color.
 vec4 RayTracer( Ray ray )
 {
+	//첫 번째 교차점을 찾았다면
 	HitInfo hit;
 	if ( IntersectRay( hit, ray ) ) {
 		vec3 view = normalize( -ray.dir );
 		vec4 clr = Shade( hit.mtl, hit.position, hit.normal, view, true);
 
-		// Compute reflections
+		//최대 반사 횟수에 따라 반사 수행
 		vec4 k_s = hit.mtl.k_s;
 		for ( int bounce=0; bounce<uBounceLimit; bounce++ ) {
 
 			if ( hit.mtl.k_s.r + hit.mtl.k_s.g + hit.mtl.k_s.b <= 0.0 ) break;
 			
-			Ray r;	// this is the reflection ray
-			HitInfo h;	// reflection hit info
+			Ray r;
+			HitInfo h;
 						
 			r.pos = hit.position.xyz;
 			r.dir = reflect(ray.dir, hit.normal);
-			float rIntensity = pow(0.8, bounce);
+			float rIntensity = pow(0.8, bounce); //반사 수행 후 강도를 줄임
 
+			//교차점을 찾을 때 마다 반사 색 누적
 			if ( IntersectRay( h, r ) ) {
 				vec3 view_reflected = normalize(-r.dir);
 				vec4 clr_reflected = Shade(h.mtl, h.position, h.normal, view_reflected, false);
@@ -147,26 +149,26 @@ vec4 RayTracer( Ray ray )
 				ray = r;
 				k_s = hit.mtl.k_s * rIntensity * 0.2;
 			} 
-			
-			else {
-				// The refleciton ray did not intersect with anything,
-				// so we are using the environment color
 
+			//더 이상 교차점을 찾지 못했다면 환경 색 반환
+			else {
 				clr += k_s * texture(uCube, vec3(1, -1, 1) * r.dir);
-				break;	// no more reflections
+				break;
 			}
 		}
 
-		return clr;	// return the accumulated color, including the reflections
+		return clr;
 	} 
-	
+
+	//아무 교차점을 찾지 못했다면 환경 색 반환
 	else {
-		return texture(uCube, vec3(1,-1,1)*ray.dir);	// return the environment color
+		return texture(uCube, vec3(1,-1,1)*ray.dir);
 	}
 }
 
 void main()
 {
+	//일반적인 phong shading
 	if(uDrawingMode == 0) 
 	{
 		vec3 N = normalize(N3); 
@@ -185,6 +187,8 @@ void main()
 
 		fColor += uSpc*texture(uCube, vec3(1,-1,1)*dir);
 	}
+
+	//ray tracing 지원
 	else if(uDrawingMode == 1)
 	{
 		Ray r;
